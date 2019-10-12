76e0fd54d1047515dcc6b22a99866ac24268b5e7
Naomi Wiggins <naomiwiggins08@gmail.com>
2019-10-12 14:59:45 -0500

    Creating project

---
+++ QRCodeAuth-Mobile/QRCodeAuth-Mobile/QRCodeAuth-Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using QRCodeAuth_Mobile.Models;
+
+namespace QRCodeAuth_Mobile.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
