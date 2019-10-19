855c5bebe90ff27ada38f5c068cf33885e16f73d
Naomi Wiggins <naomiwiggins08@gmail.com>
2019-10-19 10:51:21 -0500

    Added CrossCurrentActivity plugin to Androind project.

--- QRCodeAuthMobile/QRCodeAuthMobile/QRCodeAuthMobile.Android/MainActivity.cs
+++ QRCodeAuthMobile/QRCodeAuthMobile/QRCodeAuthMobile.Android/MainActivity.cs
@@ -7,6 +7,8 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Plugin.CurrentActivity;
+
 namespace QRCodeAuthMobile.Droid
 {
     [Activity(Label = "QRCodeAuthMobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -22,12 +24,16 @@ namespace QRCodeAuthMobile.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-        }
+
+			CrossCurrentActivity.Current.Init(this, savedInstanceState);
+		}
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+			Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
