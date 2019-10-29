77364e2c411336e4126cd597ea3a7273c275a4b2
ortuno.marilin@outlook.com <ortuno.marilin@outlook.com>
2019-10-29 14:48:02 -0500

    Added Interface for Face Authentification.

---
+++ QRCodeAuthMobile/QRCodeAuthMobile/QRCodeAuthMobile/Interfaces/IFaceAuth.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRCodeAuthMobile.Interfaces
+{
+    interface IFaceAuth
+    {
+        void FaceAuthentication();
+    }
+}
