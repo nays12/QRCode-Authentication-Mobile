522b9ad51486bee61c9538e13ce4c18ee69b2d5d
Naomi Wiggins <naomiwiggins08@gmail.com>
2019-11-01 13:52:12 -0500

    Created services for consuming apis and created web api project

---
+++ QRCodeAuthMobile/QRCodeAuthMobile/QRCodeAuthMobile/Services/EventService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+using QRCodeAuthMobile.Models;
+
+namespace QRCodeAuthMobile.Services
+{
+	class EventService
+	{
+		private async void GetAllEvents()
+		{
+			HttpClient client = new HttpClient();
+			var response = await client.GetStringAsync("http://localhost:60933/api/Events/GetAll");
+			JsonConvert.DeserializeObject<List<Event>>(response);
+
+		}
+	}
+}
